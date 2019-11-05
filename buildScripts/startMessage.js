187f9c051e17103ca9b4aec73f82c1ae91ebac17
Seyi Odunlade <seyiodunlade1@gmail.com>
2019-11-05 20:46:15 +0100

    First commit

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,4 @@
+import chalk from 'chalk';
+//var chalk = require('chalk');
+
+console.log(chalk.green('Starting app in dev mode'));// eslint-disable-line no-console
---
+++ buildScripts/testSetup.js
@@ -0,0 +1,8 @@
+//This file isn't transpiled, so must use commonJS and ES5
+
+//Register Babel to transpile before our tests run
+require('babel-register');
+
+
+//Disable webpack features that Mocha doesn't understand
+require.extensions['.css'] = function() {};
